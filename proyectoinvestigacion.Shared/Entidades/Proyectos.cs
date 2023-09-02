a698c012f6332b1b8599c6d9e4a7f4e03a1e7852
ANA MAR�A TORRES TORRES <anatorres318676@correo.itm.edu.co>
2023-09-02 06:35:25 -0500

    Se inicia DbContext 1, tambien entidades proyectos

---
+++ proyectoinvestigacion.Shared/Entidades/Proyectos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectoinvestigacion.Shared.Entidades
+{
+    public class proyectos
+    {
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "El nombre del proyecto es requerido")]
+        public string NombrePro { get; set; } = null;
+        [Required (ErrorMessage = "la descripción es requerida")]
+        public string Descripcion { get; set; } = null;
+        public string lider { get; set; } = null; 
+        public string Area { get; set; } = null;
+        public string FeInicio { get; set; } = null;
+        public string FeFinal { get; set; } = null;
+    
+    }
+}
---
+++ proyectoinvestigacion/proyectoinvestigacion.API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using proyectoinvestigacion.Shared.Entidades;
+
+namespace proyectoinvestigacion.API.Data
+{
+    public class DataContext : DbContext
+    {
+    public DataContext(DbContextOptions<DataContext> options) : base (options) {
+        
+        }  
+    public DbSet<proyectos>Proyectos { get; set; }
+
+        public override void OnModel
+
+    }
+  
+}
+
